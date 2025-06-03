a9bb4eaaf871ad116ab5370812b935cecbb57087
Yassine Erradouani <yassine.erradouani@gmail.com>
2025-06-03 20:37:00 +0100

    update storybook config and docs

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ addons.setConfig({
   theme: {
     ...themes.dark,
     brandImage: './icon.svg',
-    brandTitle: 'Hamish Williams Components',
-    brandUrl: 'https://hamishw.com',
+    brandTitle: 'Yassine Erradouani Components',
+    brandUrl: 'https://yerradouani.com',
   },
 });
---
+++ todo.md
@@ -0,0 +1,4 @@
+add source for images for the articles page and resume page in the rigt corner of the webstie 
+fix the prject cards in the resume page
+update the project in the home page
+add the content for articles and finish the skimming throught 
